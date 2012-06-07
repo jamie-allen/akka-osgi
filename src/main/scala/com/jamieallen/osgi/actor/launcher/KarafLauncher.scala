7542728cf56421617fe4c8d462eb929851dd22c3
Jamie Allen <shinolajla@yahoo.com>
2012-06-07 16:16:07 -0700

    Code cleanup

--- src/main/scala/com/jamieallen/osgi/actor/launcher/KarafLauncher.scala
+++ src/main/scala/com/jamieallen/osgi/actor/launcher/KarafLauncher.scala
@@ -5,6 +5,10 @@ import java.util.{ HashMap, ServiceLoader }
 import org.osgi.framework.{ Bundle, BundleContext }
 import org.osgi.framework.launch.{ Framework, FrameworkFactory }
 
+/**
+ * This is a simple example of how to use an Akka actor to manage the entire
+ * OSGi container, installing and starting bundles and shutting them down.
+ */
 object KarafLauncher extends App {
   val system1 = ActorSystem()
   val sysman1 = system1.actorOf(Props[ActorMgr])
@@ -14,13 +18,16 @@ object KarafLauncher extends App {
 }
 
 class ActorMgr extends Actor {
-  // By declaring this as an option, I get safe processing throughout 
-  // this code as the Uniform Return Type Principle keeps me safe
+  // By declaring this as an option, I get safe processing throughout this code as the Uniform Return Type 
+  // Principle keeps me from having to check whether or not the Framework exists before I want to do something.
   var karaf: Option[Framework] = None
 
   override def preStart = {
     println("ActorMgr about to be started, initializing Karaf framework.")
 
+    // Note that this line, where we get an iterator and call next, drives me crazy.  However, if
+    // no factory can be loaded, an exception will be thrown with no next element in the iterator,
+    // which kills the actor.  So the behavior is actually what we want.
     val factory: FrameworkFactory = ServiceLoader.load(classOf[FrameworkFactory], getClass.getClassLoader).iterator.next
     karaf = Some(factory.newFramework(new HashMap[String, AnyRef]()))
     karaf map (_.start)
@@ -35,14 +42,11 @@ class ActorMgr extends Actor {
       "file:///Users/jamie/sandbox/akka-osgi/lib/akka-actor-2.1-SNAPSHOT.jar",
       "file:///Users/jamie/sandbox/akka-osgi/target/scala-2.9.2/akka-osgi-poc_2.9.2-0.1-SNAPSHOT.jar")
 
-    // Install and start each bundle.  Note that mapping over 
-    // the BundleContext provides safety if there is none.
+    // Install and start each bundle; the for comprehension over the Option[BundleContext] provides safety if none exists.
     for {
       bc <- bundleContext
       bTI <- bundlesToInstall
     } bc.installBundle(bTI).start
-
-    //    bundleContext map (bc => for (bTI <- bundlesToInstall) bc.installBundle(bTI).start)
 
     println("Context started, bundles installed and started: ")
     karaf map (_.getBundleContext.getBundles.foreach(println))
